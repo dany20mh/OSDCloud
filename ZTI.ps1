e09f670e1efd2fbd7f0223f63f76218fbe0f8b3b
Danial <dany20mh@gmail.com>
2021-04-29 14:38:18 -0400

    Add files via upload

---
+++ ZTI.ps1
@@ -0,0 +1,17 @@
+Write-Host  -ForegroundColor Cyan "Starting Clark Cloud Imageing ..."
+Start-Sleep -Seconds 5
+
+# Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Start Zero Touch 
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
