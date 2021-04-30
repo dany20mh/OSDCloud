ccc12f01789d48353bf6359871f4b7e16e5f6449
Danial <dany20mh@gmail.com>
2021-04-30 13:41:07 -0400

    Add files via upload

--- ZTI.ps1
+++ ZTI.ps1
@@ -2,12 +2,6 @@
 Write-Host -ForegroundColor Green "Starting Clark Imaging ZTI"
 Start-Sleep -Seconds 5
 
-# Updating OSD 
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force
-Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force
-
 # Start the Script
 Write-Host -ForegroundColor Green "Start Cloud Image..."
 Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
