3a9a354273ac4d2d99ffb757930584dea8c9b94c
Danial <dany20mh@gmail.com>
2021-05-09 09:59:52 -0400

    Image File

--- ZTI.ps1
+++ ZTI.ps1
@@ -4,7 +4,7 @@ Start-Sleep -Seconds 5
 
 # Start the Script
 Write-Host -ForegroundColor Green "Start Cloud Image..."
-Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI 
+Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/20H2/install_20H2_2021_04_(19042.928).esd?AWSAccessKeyId=AKIAJ6POTXPHUP2EPBXA&Expires=1715135567&Signature=s8kbsn95UYNj%2BOTG%2FYAl845GlR4%3D"
 
 # Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
