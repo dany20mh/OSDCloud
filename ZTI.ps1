b98315891da3c8fb6b2459e8461baecd1db00526
Danial <dany20mh@gmail.com>
2021-05-09 11:15:40 -0400

    Add update module

--- ZTI.ps1
+++ ZTI.ps1
@@ -2,9 +2,13 @@
 Write-Host -ForegroundColor Green "Starting Clark Imaging ZTI"
 Start-Sleep -Seconds 5
 
+# Updating Module
+Install-Module OSD -Force
+Import-Module OSD -Force
+
 # Start the Script
 Write-Host -ForegroundColor Green "Start Cloud Image..."
-Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/20H2/install_20H2_2021_04_(19042.928).esd?AWSAccessKeyId=AKIAJ6POTXPHUP2EPBXA&Expires=1715135567&Signature=s8kbsn95UYNj%2BOTG%2FYAl845GlR4%3D"
+Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
 
 # Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
