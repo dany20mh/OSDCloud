4fc754e8b1b95aa143ec3026ed22c7dec468f9e0
Danial <dany20mh@gmail.com>
2021-05-10 14:38:58 -0400

    S3 Bucket File

--- ZTI.ps1
+++ ZTI.ps1
@@ -9,7 +9,7 @@ Start-Sleep -Seconds 5
 # Start the Script
 Write-Host -ForegroundColor Green "Start Cloud Image..."
 #Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
-Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://clarkconstruction.box.com/shared/static/d34vssc5ogwnfthq79w4q6k3sxe73spx.esd"
+Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_20H2_2021_04_19042_928.esd"
 
 # Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
