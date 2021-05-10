b13961945a4325bb4853a92712eb60344701c1f0
Danial <dany20mh@gmail.com>
2021-05-10 10:14:13 -0400

    Change Image URL

--- ZTI.ps1
+++ ZTI.ps1
@@ -3,15 +3,16 @@ Write-Host -ForegroundColor Green "Starting Clark Imaging ZTI"
 Start-Sleep -Seconds 5
 
 # Updating Module
-Install-Module OSD -Force
-Import-Module OSD -Force
+# Install-Module OSD -Force
+# Import-Module OSD -Force
 
 # Start the Script
 Write-Host -ForegroundColor Green "Start Cloud Image..."
-Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+#Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://clarkconstruction.box.com/shared/static/d34vssc5ogwnfthq79w4q6k3sxe73spx.esd"
 
 # Restart from WinPE
-Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
+Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
 
 wpeutil reboot
