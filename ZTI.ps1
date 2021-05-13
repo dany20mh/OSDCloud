4af6155b327cf7ec6ec52bfb37a7917b88f4c3b9
Danial <dany20mh@gmail.com>
2021-05-13 11:28:02 -0400

    Comment and Modification

--- ZTI.ps1
+++ ZTI.ps1
@@ -1,15 +1,27 @@
+<#
+ # Description : Script to Run Cloud Image
+ # Created : 05/10/2021 by Danial
+ #>
+
+Write-Host "================================================================" -ForegroundColor Blue
+Write-Host "================ Clark Construction Group, LLC =================" -ForegroundColor Blue
+Write-Host "================ Cloud Image Deployment Script =================" -ForegroundColor Blue
+Write-Host "================================================================" -ForegroundColor Blue
+
 # Starting the Imaging
-Write-Host -ForegroundColor Green "Starting Clark Imaging ZTI"
+Write-Host ""
+Write-Host "================================================================" -ForegroundColor Blue
+Write-Host "===================== Starting Imaging ZTI =====================" -ForegroundColor Blue
+Write-Host "============= Edition - 20H2 == Build - 19402.985 ==============" -ForegroundColor Blue
+Write-Host "================================================================" -ForegroundColor Blue
 Start-Sleep -Seconds 5
 
 # Updating Module
 # Install-Module OSD -Force
 # Import-Module OSD -Force
 
-# Start the Script
-Write-Host -ForegroundColor Green "Start Cloud Image..."
-#Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
-Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_20H2_2021_04_19042_928.esd"
+# Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_20H2_2021_05_19042_985.esd"
 
 # Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
