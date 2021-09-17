3009d4f7f8dd62343a25012eb5a6fe70309be898
Danial <dany20mh@gmail.com>
2021-09-17 10:47:51 -0400

    Update to 19042.1237

--- ZTI.ps1
+++ ZTI.ps1
@@ -9,7 +9,7 @@ Write-Host "===================== Clark Construction Group, LLC ================
 Write-Host "===================== Cloud Image Deployment Script =====================" -ForegroundColor Cyan
 Write-Host "=========================================================================" -ForegroundColor Cyan
 Write-Host "========================== Starting Imaging ZTI =========================" -ForegroundColor Cyan
-Write-Host "================== Edition - 20H2 == Build - 19042.1165 =================" -ForegroundColor Cyan
+Write-Host "================== Edition - 20H2 == Build - 19042.1237 =================" -ForegroundColor Cyan
 Write-Host "=========================================================================" -ForegroundColor Cyan
 Start-Sleep -Seconds 5
 
@@ -18,7 +18,7 @@ Install-Module OSD -Force
 Import-Module OSD -Force
 
 # Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
-Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_20H2_2021_08_19042_1165.esd"
+Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_20H2_2021_09_19042_1237.esd"
 
 # Set Drive Lable Name
 Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
