f63c7e9ab00866c92a684113590d5d8be1a5d933
Danial <danial@duck.com>
2022-12-02 12:12:30 -0500

    Update ZTI2.ps1

--- ZTI2.ps1
+++ ZTI2.ps1
@@ -16,6 +16,35 @@ Start-Sleep -Seconds 5
 Install-Module OSD -Force
 Import-Module OSD -Force
 
+# Select Deployment Method
+$actionChoice = [System.Management.Automation.Host.ChoiceDescription[]](@(
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&Hybrid", "Hybrid Joined Machine"))
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&AAD", "AzureAD Joined Machine")),
+    (New-Object System.Management.Automation.Host.ChoiceDescription("&Travel", "AzureAD Joined Machine for Travel"))
+))
+
+$action = $Host.Ui.PromptForChoice("Deployment Method", "Select a Deployment method to perform imaging", $actionChoice, 0)
+
+If ( $action -eq 0 ) {
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+    Write-Host "========================== Hybrid Deployment ============================" -ForegroundColor Cyan
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+} 
+
+If ( $action -eq 1 ) {
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+    Write-Host "========================= AzureAD Deployment ============================" -ForegroundColor Cyan
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+    Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/fli8o80p0wccwj688zhmixgjzk15wzom.json" -OutFile X:\OSDCloud\Autopilot\Profiles\AutopilotProfile.json
+} 
+
+If ( $action -eq 2 ) {
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+    Write-Host "========================= Travel PC Deployment ==========================" -ForegroundColor Cyan
+    Write-Host "=========================================================================" -ForegroundColor Cyan
+    Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/uzfltkr8mllyd4t7xld64fqendtbgt6j.json" -OutFile X:\OSDCloud\Autopilot\Profiles\AutopilotProfile.json
+} 
+
 # Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 21H2 -OSEdition Enterprise -ZTI
 Start-OSDCloud -Product NODRIVER -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_22H2_2022_11_22621_819.esd"
 
