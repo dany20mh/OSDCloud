99d5a5f48a5ea386f7a9593064fff086ca2fdf11
Danial <dany20mh@gmail.com>
2024-03-11 11:04:21 -0400

    Testing Update

--- ZTI2.ps1
+++ ZTI2.ps1
@@ -16,6 +16,7 @@ Start-Sleep -Seconds 5
 # Updating Module
 #Install-Module OSD -Force
 #Import-Module OSD -Force
+iex (irm functions.garytown.com)
 
 # Select Deployment Method
 $actionChoice = [System.Management.Automation.Host.ChoiceDescription[]](@(
@@ -23,7 +24,7 @@ $actionChoice = [System.Management.Automation.Host.ChoiceDescription[]](@(
     (New-Object System.Management.Automation.Host.ChoiceDescription("&AAD", "AzureAD Joined Machine")),
     (New-Object System.Management.Automation.Host.ChoiceDescription("&Travel", "AzureAD Joined Machine for Travel"))
     (New-Object System.Management.Automation.Host.ChoiceDescription("&Donation PC", "Regular Image for Donation PC"))
-))
+    ))
 
 $action = $Host.Ui.PromptForChoice("Deployment Method", "Select a Deployment method to perform imaging", $actionChoice, 0)
 
@@ -84,6 +85,13 @@ If ( $action -eq 3 ) {
         SkipODT                    = $true
         SkipOOBEDeploy             = $true
         ZTI                        = $true
+        DriverPackName             = 'NONE'
+        WindowsUpdateDrivers       = $false
+        HPIADrivers                = $true
+        HPIAFirmware               = $true
+        HPTPMUpdate                = $true
+        HPBIOSUpdate               = $true
+
     }
     Start-OSDCloud
 
@@ -98,6 +106,16 @@ If ( $action -eq 3 ) {
 } 
 
 # Start-OSDCloud -Product NODRIVER -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -ZTI
+$Global:StartOSDCloudGUI = [ordered]@{
+    ZTI                        = $true
+    DriverPackName             = 'NONE'
+    WindowsUpdateDrivers       = $false
+    HPIADrivers                = $true
+    HPIAFirmware               = $true
+    HPTPMUpdate                = $true
+    HPBIOSUpdate               = $true
+
+}
 Start-OSDCloud -ZTI -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_23H2_2024_02_22631_3155_PRO.esd"
 
 
