16255597fddcff6b012602340430fc44bf7be41a
Danial <dany20mh@gmail.com>
2024-05-20 15:53:24 -0400

    New Update

--- ZTI2.ps1
+++ ZTI2.ps1
@@ -1,60 +1,56 @@
 <#
- # Description : Script to Run Cloud Image
- # Created : 05/10/2021 by Danial
+ # Description: Script to Run Cloud Image
+ # Created: 05/10/2021 by Danial
  #>
 
-# Making sure the module is up to date
+# Clear the host to ensure a clean environment
 Clear-Host
-$version = (Get-Module -ListAvailable OSD) | Sort-Object Version -Descending  | Select-Object Version -First 1
-$stringver = $version | Select-Object @{n = 'ModuleVersion'; e = { $_.Version -as [string] } }
-$a = $stringver | Select-Object Moduleversion -ExpandProperty Moduleversion
-$psgalleryversion = Find-Module -Name OSD | Sort-Object Version -Descending | Select-Object Version -First 1
-$onlinever = $psgalleryversion | Select-Object @{n = 'OnlineVersion'; e = { $_.Version -as [string] } }
-$b = $onlinever | Select-Object OnlineVersion -ExpandProperty OnlineVersion
- 
-if ([version]"$a" -ge [version]"$b") {}
-else {
+
+# Retrieve the currently installed version of the OSD module
+$currentVersion = (Get-Module -ListAvailable OSD | Sort-Object Version -Descending | Select-Object -First 1).Version
+
+# Retrieve the latest available version of the OSD module from the PowerShell gallery
+$latestVersion = (Find-Module -Name OSD | Sort-Object Version -Descending | Select-Object -First 1).Version
+
+# Compare versions and install/update the OSD module if necessary
+if ($currentVersion -lt $latestVersion) {
     Install-Module -Name OSD -Force
-    Import-Module OSD                
+    Import-Module OSD
 }
 
-# Final check for Module 
-$version = (Get-Module -ListAvailable OSD) | Sort-Object Version -Descending  | Select-Object Version -First 1
-$stringver = $version | Select-Object @{n = 'ModuleVersion'; e = { $_.Version -as [string] } }
-$a = $stringver | Select-Object Moduleversion -ExpandProperty Moduleversion
+# Final check for the OSD module version
+$currentVersion = (Get-Module -ListAvailable OSD | Sort-Object Version -Descending | Select-Object -First 1).Version
 
-if ([version]"$a" -ge [version]'24.3.10.1') {
+if ($currentVersion -ge [version]'24.3.10.1') {
 
     Clear-Host
-    # Starting the Imaging
+    # Display startup message
     Write-Host "=========================================================================" -ForegroundColor Cyan
     Write-Host "===================== Cloud Image Deployment Script =====================" -ForegroundColor Cyan
     Write-Host "=========================================================================" -ForegroundColor Cyan
     Write-Host "========================== Starting Imaging ZTI =========================" -ForegroundColor Cyan
-    Write-Host "================= Edition - 23H2 == Build - 22631.3296 ==================" -ForegroundColor Cyan
+    Write-Host "================= Edition - 23H2 == Build - 22631.3593 ==================" -ForegroundColor Cyan
     Write-Host "=========================================================================" -ForegroundColor Cyan
     Start-Sleep -Seconds 5
 
-    # Select Deployment Method
-    $actionChoice = [System.Management.Automation.Host.ChoiceDescription[]](@(
-    (New-Object System.Management.Automation.Host.ChoiceDescription("&Hybrid", "Hybrid Joined Machine"))
-    (New-Object System.Management.Automation.Host.ChoiceDescription("&AAD", "AzureAD Joined Machine")),
-    (New-Object System.Management.Automation.Host.ChoiceDescription("&Travel", "AzureAD Joined Machine for Travel"))
-    (New-Object System.Management.Automation.Host.ChoiceDescription("&Donation PC", "Regular Image for Donation PC"))
-        ))
+    # Deployment method choices
+    $actionChoice = [System.Management.Automation.Host.ChoiceDescription[]]@(
+        (New-Object System.Management.Automation.Host.ChoiceDescription("&Hybrid", "Hybrid Joined Machine")),
+        (New-Object System.Management.Automation.Host.ChoiceDescription("&EntraID", "EntraID Joined Machine")),
+        (New-Object System.Management.Automation.Host.ChoiceDescription("&Travel", "EntraID Joined Machine for Travel")),
+        (New-Object System.Management.Automation.Host.ChoiceDescription("&Donation PC", "Regular Image for Donation PC"))
+    )
 
+    # Prompt user to select a deployment method
     $action = $Host.Ui.PromptForChoice("Deployment Method", "Select a Deployment method to perform imaging", $actionChoice, 0)
 
-    $Global:StartOSDCloudGUI = [ordered]@{
+    # Base configuration for OSDCloudGUI with default values
+    $baseConfig = @{
         ZTI                  = $true
         HPIADrivers          = $true
         HPIAFirmware         = $true
         HPTPMUpdate          = $true
         HPBIOSUpdate         = $true
-        OSName               = 'Windows 11 23H2 x64'
-        OSEdition            = 'Pro'
-        OSActivation         = 'Retail'
-        OSLanguage           = 'en-us'
         updateDiskDrivers    = $true
         updateFirmware       = $true
         updateNetworkDrivers = $true
@@ -64,80 +60,82 @@ if ([version]"$a" -ge [version]'24.3.10.1') {
         WindowsUpdateDrivers = $true
     }
 
-    If ( $action -eq 0 ) {
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Write-Host "========================== Hybrid Deployment ============================" -ForegroundColor Cyan
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-    } 
+    # Specific configuration for Donation PC Deployment
+    $donationPCConfig = @{
+        ApplyManufacturerDrivers   = $false
+        ApplyCatalogDrivers        = $false
+        ApplyCatalogFirmware       = $false
+        AutopilotJsonChildItem     = $false
+        AutopilotJsonItem          = $false
+        AutopilotJsonName          = $false
+        AutopilotJsonObject        = $false
+        AutopilotOOBEJsonChildItem = $false
+        AutopilotOOBEJsonItem      = $false
+        AutopilotOOBEJsonName      = $false
+        AutopilotOOBEJsonObject    = $false
+        ImageFileFullName          = $false
+        ImageFileItem              = $false
+        ImageFileName              = $false
+        OOBEDeployJsonChildItem    = $false
+        OOBEDeployJsonItem         = $false
+        OOBEDeployJsonName         = $false
+        OOBEDeployJsonObject       = $false
+        Restart                    = $false
+        SkipAutopilot              = $true
+        SkipAutopilotOOBE          = $true
+        SkipODT                    = $true
+        SkipOOBEDeploy             = $true 
+        OSName                     = 'Windows 11 23H2 x64'
+        OSEdition                  = 'Pro'
+        OSActivation               = 'Retail'
+        OSLanguage                 = 'en-us'
+    }
 
-    If ( $action -eq 1 ) {
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Write-Host "========================= AzureAD Deployment ============================" -ForegroundColor Cyan
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/fli8o80p0wccwj688zhmixgjzk15wzom.json" -OutFile X:\OSDCloud\Config\AutopilotJSON\AutopilotProfile.json
-    } 
+    # Apply specific configuration based on user selection
+    if ($action -eq 3) {
+        $Global:StartOSDCloudGUI = $baseConfig + $donationPCConfig
+    }
+    else {
+        $Global:StartOSDCloudGUI = $baseConfig
+    }
 
-    If ( $action -eq 2 ) {
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Write-Host "========================= Travel PC Deployment ==========================" -ForegroundColor Cyan
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/uzfltkr8mllyd4t7xld64fqendtbgt6j.json" -OutFile X:\OSDCloud\Config\AutopilotJSON\AutopilotProfile.json
-    } 
 
-    If ( $action -eq 3 ) {
-        Write-Host "=========================================================================" -ForegroundColor Cyan
-        Write-Host "======================== Donation PC Deployment =========================" -ForegroundColor Cyan
-        Write-Host "=========================================================================" -ForegroundColor Cyan
+    # Common deployment message
+    $deploymentMessages = @(
+        "========================== Hybrid Deployment ============================",
+        "========================= AzureAD Deployment ============================",
+        "========================= Travel PC Deployment ==========================",
+        "======================== Donation PC Deployment ========================="
+    )
 
-        $Global:StartOSDCloudGUI = [ordered]@{
-            ApplyManufacturerDrivers   = $false
-            ApplyCatalogDrivers        = $false
-            ApplyCatalogFirmware       = $false
-            AutopilotJsonChildItem     = $false
-            AutopilotJsonItem          = $false
-            AutopilotJsonName          = $false
-            AutopilotJsonObject        = $false
-            AutopilotOOBEJsonChildItem = $false
-            AutopilotOOBEJsonItem      = $false
-            AutopilotOOBEJsonName      = $false
-            AutopilotOOBEJsonObject    = $false
-            ImageFileFullName          = $false
-            ImageFileItem              = $false
-            ImageFileName              = $false
-            OOBEDeployJsonChildItem    = $false
-            OOBEDeployJsonItem         = $false
-            OOBEDeployJsonName         = $false
-            OOBEDeployJsonObject       = $false
-            Restart                    = $false
-            SkipAutopilot              = $true
-            SkipAutopilotOOBE          = $true
-            SkipODT                    = $true
-            SkipOOBEDeploy             = $true        
+    Write-Host $deploymentMessages[$action] -ForegroundColor Cyan
+
+    # Handle deployment based on user selection
+    switch ($action) {
+        1 {
+            Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/fli8o80p0wccwj688zhmixgjzk15wzom.json" -OutFile X:\OSDCloud\Config\AutopilotJSON\AutopilotProfile.json
         }
-        Start-OSDCloud
+        2 {
+            Invoke-WebRequest -Uri "https://clarkconstruction.box.com/shared/static/uzfltkr8mllyd4t7xld64fqendtbgt6j.json" -OutFile X:\OSDCloud\Config\AutopilotJSON\AutopilotProfile.json
+        }
+        3 {
+            Start-OSDCloud
+        }
+    }
 
-        # Set Drive Lable Name
-        Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
-
-        # Restart from WinPE
-        Write-Host -ForegroundColor Cyan "Restarting in 10 seconds!"
-        Start-Sleep -Seconds 10
-
-        wpeutil reboot
-    } 
-
-    Start-OSDCloud -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_23H2_2024_03_22631_3296_PRO.esd"
-
-    # Set Drive Lable Name
+    # Common deployment tasks for non-donation PCs
+    if ($action -ne 3) {
+        Start-OSDCloud -ImageFileUrl "https://ccgsoftdist.s3.amazonaws.com/Kaseya/Windows10/install_23H2_2024_05_22631_3593_PRO.esd"
+    }
+    # Set drive label name
     Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
 
     # Restart from WinPE
-    Write-Host -ForegroundColor Cyan "Restarting in 10 seconds!"
+    Write-Host "Restarting in 10 seconds!" -ForegroundColor Cyan
     Start-Sleep -Seconds 10
-
     wpeutil reboot
 
 }
 else {
-    Write-Host -ForegroundColor Cyan "The required module is not loaded. Please check the network connection and try again!"
+    Write-Host "The required module is not loaded. Please check the network connection and try again!" -ForegroundColor Cyan
 }
